5aaf4bd2ebb1c31406be2bd7f401393680c4d679
rajdeepsr2007 <rajdeepsr2007@gmail.com>
2021-04-10 10:57:42 +0530

    Storing the token

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -9,10 +9,11 @@ export const authStart = () => {
     }
 }
 
-export const authSuccess = (authData) => {
+export const authSuccess = (token , userId) => {
     return {
         type : actionTypes.AUTH_SUCEESS ,
-        authData : authData
+        idToken : token ,
+        userId : userId
     }
 }
 
@@ -44,7 +45,7 @@ export const auth = (email,password , isSignup) => {
         axios.post( url , authData )
              .then( response => {
                 console.log(response);
-                dispatch( authSuccess( response.data ) )
+                dispatch( authSuccess( response.data.idToken , response.data.localId ) )
              } )
              .catch( error => {
                 console.log(error);
