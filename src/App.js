956d5690d9175efab5731be353c628501d57a863
rajdeepsr2007 <rajdeepsr2007@gmail.com>
2021-04-12 14:20:05 +0530

    .

--- src/App.js
+++ src/App.js
@@ -23,24 +23,27 @@ class App extends Component {
     let routes = (
       <Switch>
         <Route path="/auth" component={Auth} />
-        <Route path="/logout" component={Logout} />
         <Route path="/" exact component={BurgerBuilder} />
-        <Redirect to="/" />
       </Switch>
     ) ;
+
+    if( this.props.isAuthenticated ){
+      routes = (
+        <Switch>
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/" exact component={BurgerBuilder} />
+        </Switch>
+      )
+    }
 
     
 
     return (
       <div>
         <Layout>
-          <Switch>
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" component={Orders} />
-            <Route path="/auth" component={Auth} />
-            <Route path="/logout" component={Logout} />
-            <Route path="/" exact component={BurgerBuilder} />
-          </Switch>
+          { routes }
         </Layout>
       </div>
     );
@@ -61,5 +64,5 @@ const mapDispatchToProps = dispatch => {
 
 //Whenever having error with routing , Component having Router wrapped inside connect
 //Wrap connect with withRouter
-export default withRouter(connect( null , mapDispatchToProps)(App)) ;
+export default withRouter(connect( mapStateToProps , mapDispatchToProps)(App)) ;
 
