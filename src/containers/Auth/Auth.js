a90f8df4ffa6320055cd30e8cc55bfc34904e7a5
rajdeepsr2007 <rajdeepsr2007@gmail.com>
2021-04-12 18:59:32 +0530

    Share validation method

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -13,7 +13,7 @@ import classes from './Auth.css' ;
 import Spinner from '../../components/UI/Spinner/Spinner' ;
 import { Redirect } from 'react-router';
 
-import {updateObject} from '../../shared/utility' ;
+import {checkValidity, updateObject} from '../../shared/utility' ;
 
 class Auth extends Component {
 
@@ -60,37 +60,6 @@ class Auth extends Component {
         }
     }
 
-    checkValidity(value, rules) {
-        let isValid = true;
-        if (!rules) {
-            return true;
-        }
-        
-        if (rules.required) {
-            isValid = value.trim() !== '' && isValid;
-        }
-
-        if (rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid
-        }
-
-        if (rules.maxLength) {
-            isValid = value.length <= rules.maxLength && isValid
-        }
-
-        if (rules.isEmail) {
-            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-            isValid = pattern.test(value) && isValid
-        }
-
-        if (rules.isNumeric) {
-            const pattern = /^\d+$/;
-            isValid = pattern.test(value) && isValid
-        }
-
-        return isValid;
-    }
-
     inputChangedHandler = ( event , controlName ) => {
        
         // const isValid = this.checkValidity( event.target.value , this.state.controls[controlName].validation ) ;
@@ -112,7 +81,7 @@ class Auth extends Component {
                 [controlName] : updateObject(
                                 this.state.controls[controlName] , {
                                 value : event.target.value ,
-                                valid : this.checkValidity( event.target.value , this.state.controls[controlName].validation ),
+                                valid : checkValidity( event.target.value , this.state.controls[controlName].validation ),
                                 touched : true
                 } )
             }
